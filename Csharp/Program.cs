540ad27682767b91f61af3568158bceb7dfd9a32
Vassilis <stavreas@hotmail.com>
2014-04-23 03:36:26 +0300

    Commit CSharp Folder

---
+++ Csharp/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace tabPreproc1
+{
+
+
+
+    class Ident
+    {
+        public int length = 0;
+        public int position = 0;
+    }
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string filepath = args[0];
+            identInLines(filepath);
+
+        }
+
+
+
+        static void identInLines(string filepath)
+        {
+            string input = "";
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+
+                input = sr.ReadToEnd();
+                sr.Close();
+                //input += Environment.NewLine + " ";
+            }
+
+            
+            var matchesLines = Regex.Matches(input, @"(.+\w+.+)" + Environment.NewLine, RegexOptions.Multiline);
+            string[] lines = new string[matchesLines.Count +1];
+           // lines[lines.Length - 1] = " ";
+            int index = 0; 
+            int stored = 0;    
+            int measured = 0;
+            foreach (Match line in matchesLines)
+            {
+
+                measured = line.Value.Count(p => p == '\t');
+               // measured = Regex.Match(line.Value, @"^(\t)+", RegexOptions.Multiline).Captures.Count;
+                if (measured > stored)
+                {
+                    lines[index] = ">" + line.Value;
+                    stored++;
+                }
+                else if (measured < stored)
+                {
+                    string dedentString = "";
+                    for(int i = 0; i < stored - measured; i++)
+                    {
+                        dedentString += "<";
+                    }
+                    lines[index] = line.Value;
+                    lines[index - 1] = lines[index - 1].TrimEnd();
+                    while ( lines[index - 1].Substring(lines[index - 1].Length-2, 2) == Environment.NewLine )
+                    {
+                        lines[index - 1] = lines[index - 1].Trim(Environment.NewLine.ToCharArray());
+                    }
+                    
+                    lines[index - 1] =  lines[index - 1] + dedentString + Environment.NewLine;
+
+                    stored = measured;
+                }
+                else
+                {
+                    lines[index] = line.Value;
+                }
+                index++;
+
+            }
+            for (int j = 0; j < stored; j++)
+            {
+                lines[lines.Length - 1] += "<" + lines[lines.Length - 1];
+            }
+
+            string output = "";
+
+            output = String.Concat(lines);
+
+            StreamWriter sw = new StreamWriter(filepath);
+            sw.Write(output);
+            sw.Close();
+                        
+        }
+
+
+     
+
+
+
+    }
+}

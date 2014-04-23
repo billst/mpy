833bea3a947bb0cce2dd487880a0157910ede455
Vassilis <stavreas@hotmail.com>
2014-04-23 05:18:37 +0300

    Commit Java Parser folder - file

---
+++ Java_parser/LexerTest1.java
@@ -0,0 +1,44 @@
+import java.io.FileReader;
+import java.io.PushbackReader;
+
+//import bill.minipython2.lexer.Lexer;
+//import bill.minipython2.node.Token;
+import bill.minipython5_indented.lexer.Lexer;
+import bill.minipython5_indented.node.Start;
+import bill.minipython5_indented.node.Token;
+import bill.minipython5_indented.parser.Parser;
+import bill.minipython5_indented.*;
+
+public class LexerTest1
+{
+  public static void main(String[] args)
+  {
+    try
+    {
+      Lexer lexer =
+        new Lexer(
+        new PushbackReader(
+        new FileReader(args[0].toString()), 1024));
+
+      Token token = lexer.next();
+      while ( ! token.getText().equals("") )
+      { 
+        System.out.print(token);
+        token = lexer.next(); 
+       
+      }
+      Lexer lexer2 =
+    	        new Lexer(
+    	        new PushbackReader(
+    	        new FileReader(args[0].toString()), 1024));
+      Parser parser = new Parser(lexer2);      
+      //bill.minipython5_indented.node.EOF e = new bill.minipython5_indented.node.EOF(1,2); 
+      Start  ast = parser.parse(); 
+
+    }
+    catch (Exception e)
+    {
+      System.err.println(e);
+    }
+  }
+}

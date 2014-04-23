71a5846736dab5a27e87f3ae9d25ea817765e469
Vassilis <stavreas@hotmail.com>
2014-04-23 05:14:59 +0300

    Commit Examples folder - file

---
+++ examples/example2.1.py
@@ -0,0 +1,37 @@
+myStrVar = "hello world"
+myIntVar = 3
+def add(x,y):
+>      return x + y
+<
+def fib(n):
+>   a = 3 
+    b = [1,2]
+	for a in b:
+>		print a
+		a = b
+		b =  a  + b
+ <
+ <
+ def fifcmpl():
+>  if add(1,2) <= add(1,2)+1:
+>       print add(1,2)+1, " is grater"
+<
+<
+def findmax(x,y):
+>	if x >= y:
+>		return x
+<
+	if y >= x:
+>		return y
+<
+<
+def funcwithdef(name,university="aueb"):
+>	print name, " studies in ", university
+<
+print add(add(1,1),3) 
+w = fib(5)
+print w + findmax(1,2)
+l=[0,1,"hello","world"]
+for k in l:
+>	print k
+<
